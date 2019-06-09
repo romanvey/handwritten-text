f5f46bed7c91e71c4ba565fda44afdf0aceabee9
borsukvasyl <v.borsuk2000@gmail.com>
2019-06-09 17:18:58 +0300

    added flask app

--- src/deploy/app/app.py
+++ src/deploy/app/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+import yaml
+import json
+from http import HTTPStatus
+from flask import Flask, render_template, request, Response
+
+from contrib.generate import HWGenerator
 
 application = Flask(__name__)
+
+with open("config.yaml", "r") as f:
+    config = yaml.load(f)
+model = HWGenerator(config["model"])
+
 
 @application.route("/")
 def index():
     return render_template("index.html")
 
+
+@application.route('/api', methods=['POST'])
+def api():
+    body = request.get_json()
+    try:
+        svg_img = model(body["text"], style=body["style"])
+        return create_json_response(response={"img": svg_img}, status=HTTPStatus.OK)
+    except Exception as e:
+        return create_json_response(response=str(e), status=HTTPStatus.BAD_REQUEST)
+
+
+def create_json_response(status, response):
+    return Response(response=json.dumps(response), status=status, mimetype='application/json')
+
+
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=80)
+    application.run(host="0.0.0.0", port=8000)
