c315305b91509cab8f2e7d11a2da915cfa84e11c
Roman Vey <roman.vey@gmail.com>
2019-06-11 23:16:09 +0300

    port changed to 8000 for local build

--- local_build.sh
+++ local_build.sh
@@ -1,4 +1,4 @@
 cp -r src/contrib/ src/deploy/app
-cd src/deploy/app && PYTHONPATH=../../ /home/roman/miniconda3/envs/ds/bin/python -m gunicorn.app.wsgiapp -b 0.0.0.0:80 app
+cd src/deploy/app && PYTHONPATH=../../ /home/roman/miniconda3/envs/ds/bin/python -m gunicorn.app.wsgiapp -b 0.0.0.0:8000 app
 cd -
 rm -rf src/deploy/app/contrib
