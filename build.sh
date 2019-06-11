70f6f77e15467b6901ff8ff700486fd118d15bb8
Roman Vey <roman.vey@gmail.com>
2019-06-11 23:12:11 +0300

    ports changed

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 docker build -t flask_gunicorn_app_image src -f src/deploy/app/Dockerfile
-docker run -d --name flask_gunicorn_app -p 8000:8000 flask_gunicorn_app_image
+docker run -d --name flask_gunicorn_app -p 80:8000 flask_gunicorn_app_image
